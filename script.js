fa523fca0e3ca8c078c05de4c247c2e99e73f07e
Anitha chowdary <130220172+anitha-anii@users.noreply.github.com>
2023-07-02 19:00:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,37 @@
-function submitForm() {
-  var username = document.getElementById("username").value;
-  var password = document.getElementById("password").value;
-  var rememberMe = document.getElementById("checkbox").checked;
+var loginForm = document.getElementById('loginForm');
+var usernameInput = document.getElementById('username');
+var passwordInput = document.getElementById('password');
+var rememberMeCheckbox = document.getElementById('rememberMe');
+var submitButton = document.getElementById('submit');
+var existingUserButton = document.getElementById('existing');
 
-  if (rememberMe) {
-    localStorage.setItem("username", username);
-    localStorage.setItem("password", password);
+submitButton.addEventListener('click', function(event) {
+  event.preventDefault();
+  
+  var username = usernameInput.value;
+  var password = passwordInput.value;
+  
+  if (rememberMeCheckbox.checked) {
+    localStorage.setItem('username', username);
+    localStorage.setItem('password', password);
   } else {
-    localStorage.removeItem("username");
-    localStorage.removeItem("password");
+    localStorage.removeItem('username');
+    localStorage.removeItem('password');
   }
+  
+  alert('Logged in as ' + username);
+});
 
-  alert("Logged in as " + username);
+if (localStorage.getItem('username') && localStorage.getItem('password')) {
+  var existingUserButton = document.createElement('button');
+  existingUserButton.id = 'existing';
+  existingUserButton.textContent = 'Login as existing user';
+  
+  existingUserButton.addEventListener('click', function() {
+    var savedUsername = localStorage.getItem('username');
+    alert('Logged in as ' + savedUsername);
+  });
+  
+  loginForm.appendChild(existingUserButton);
 }
 
-window.onload = function() {
-  var existingUserButton = document.getElementById("existing");
-  var savedUsername = localStorage.getItem("username");
-
-  if (savedUsername) {
-    existingUserButton.style.display = "inline";
-  }
-
-  existingUserButton.onclick = function() {
-    alert("Logged in as " + savedUsername);
-  };
-};
